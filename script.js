84566fbb1f838eeb0f5d3cb22b9b7882b66b7d1c
menachem <AM0556727820@GMAIL.COM>
2024-09-11 14:54:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+//פונקציה שמקבלת מערך של מספרים ומחזירה את המערך בסדר עולה, עם המספרים הזוגיים בלבד.
+const filterAndSortEvenNumbers = (arry) => {
+    return arry.filter((x) => {return x % 2 == 0}).sort((a, b) => a-b);
+}
+
+const myArry = [1, 5, 3, 7, 2, 4, 8, 2, 10];
+
+// console.log(filterAndSortEvenNumbers(myArry));
+
+//פונקציה שמקבלת מערך של מספרים ומחזירה מערך חדש ללא מספרים כפולים.
+const filterDoubleNumbers = (arryNumbers) => {
+    const newArr = [];
+    for(const i of arryNumbers){
+        if(!newArr.includes(i)) newArr.push(i)
+    }
+    return newArr;
+}
+// console.log(filterDoubleNumbers(myArry));
+
+
+// //פונקציה שמקבלת מחרוזת ומחזירה מחרוזת שבה כל המילים מתחילות באות גדולה, פרט למילים שמסתיימות בנקודה
+// const getStringWithUpercase = (string) => {
+//     const listAll = string.split(" ");
+//     console.log(listAll)
+// } 
+// const myStr = "schcs sdhjcbdsh sdhcv. sdcbd sdcds.";
+// getStringWithUpercase(myStr);
+
+
